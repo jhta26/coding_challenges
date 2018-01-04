87824468677b9a7726b9762b5266bf29df9a6ddd
jhta26 <hsujasonf@gmail.com>
2018-01-04 12:58:04 -0800

    refactor extractUnique

--- extract_unique_chars.js
+++ extract_unique_chars.js
@@ -1,7 +1,6 @@
 // Write a JavaScript function to extract unique characters from a string.
 
 function extractUnique(string) {
-	const strArr = string.split('')
-    return Array.from(new Set(strArr)).join('')
+    return Array.from(new Set(string)).join('')
 }
 
