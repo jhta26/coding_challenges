4a7d96a8200ff04751f447d295dd542c9b9aabbb
jhta26 <hsujasonf@gmail.com>
2018-02-04 19:53:27 -0800

    fixed solution

--- binary_search.js
+++ binary_search.js
@@ -1,7 +1,10 @@
 function binarySearch(arr, n) {
     var middle = Math.floor(arr.length / 2)
     if (arr.length == 1 && arr[0] !== n) return 'notfound'
-    if (n > arr[middle]) return binarySearch(arr.slice(middle), n)
-    if (n < arr[middle]) return binarySearch(arr.slice(0, middle), n)
     if (n == arr[middle]) return n
+    if (n > arr[middle]) {
+        return binarySearch(arr.slice(middle), n)
+    } else {
+        return binarySearch(arr.slice(0, middle), n)
+    }
 }
